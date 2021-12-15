9dce55b9675f7f31c38b3a5083ec019f66eb0ada
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-12-15 14:04:47 +0100

    Add files via upload

---
+++ index.cs
@@ -0,0 +1,12 @@
+    using System;
+     
+    namespace Coding.Exercise
+    {
+        public class Exercise
+        {
+           public static int HowManyDays(DateTime d1, DateTime d2)
+            {
+                return (d2 - d1).Days;
+            }
+        }
+    }
